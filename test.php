fa8680d5ce02c7eb06042401fca9ef28dd74c31d
Muhammed Zahid <2015319@csl-student.wlv.ac.uk>
2023-10-19 15:10:02 +0100

    First commit with Alix

---
+++ test.php
@@ -0,0 +1,69 @@
+<?php
+  $num = 17;
+  echo 'hello<br>';
+  $num = "University of Wolverhampton";
+  $students = 212;
+  $module = "5cs023";
+  echo "The class $module has $students enrolled on it,,,,<br>";
+  // This is an integer
+  $distance = 42;
+  // This is a float
+  $distance = 42.0;
+  // This is a float equal to 1120
+  $num = 1.12e3;
+  //This is a float equal to 0.02
+  $num = 2e-2;
+  $initial = "A";
+  $surname = "Bergeret";
+  echo $initial." ".$surname;
+   define ("PI", 3.142);
+  // output the value of twice PI
+  echo (2 * PI);
+  $num = 1;
+   echo "num is now: $num<BR>";
+   $num = 4 + 9;
+   echo "num is now: $num<BR>";
+   /*
+   ** all the following add one to the value of $num
+   */
+   $num = $num + 1;
+   echo "num is now: $num<BR>";
+   $num += 1;
+   echo "num is now: $num<BR>";
+   $num++;
+   echo "num is now: $num<BR>";
+   /*
+   ** these subtract one from $num
+   */
+   $num = $num -1;
+   echo "num is now: $num<BR>";
+   $num -= 1;
+   echo "num is now: $num<BR>";
+   $num--;
+   echo "num is now: $num<BR>";
+   /*
+   ** double the value of $num
+   */
+   $num = 23;
+   echo "num is now: $num<BR>";
+   $num = $num * 2;
+   echo "num is now: $num<BR>";
+   $num *= 2;
+   echo "num is now: $num<BR>";
+   /*
+   ** halve the value of $num
+   */
+   $num = 100;
+   echo "num is now: $num<BR>";
+   $num = $num / 2;
+   echo "num is now: $num<BR>";
+   $num /= 2;
+   echo "num is now: $num<BR>";
+   /*
+   ** A combination 
+   */
+   $num = 8;
+   echo "num is now: $num<BR>";
+   $num = (((($num + 4) * 2)  - 6) / 3);
+   echo "num is now: $num<BR>";
+   
